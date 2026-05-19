d6d25efcea53488cd173c6f6e5892f0eb66949ce
cidy <cidywallace12@gmail.com>
2026-05-19 20:04:00 -0300

    Adicionando novo atributo ao card

--- lib/models/card.dart
+++ lib/models/card.dart
@@ -3,6 +3,11 @@ import 'package:card_dinamico/models/subItem.dart';
 class DinamiCard {
   String title;
   List<SubItem> itens;
+  bool isDeleted;
 
-  DinamiCard({required this.title, required this.itens});
+  DinamiCard({
+    required this.title,
+    required this.itens,
+    this.isDeleted = false,
+  });
 }
